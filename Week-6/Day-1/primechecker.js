b75d7ce3bb776c192625ba74f279e23900f87c67
Andrew Murphy <andrewmurphywastaken@gmail.com>
2018-05-31 20:35:28 -0400

    completed prime checker activity

--- Week-6/Day-1/primechecker.js
+++ Week-6/Day-1/primechecker.js
@@ -1,8 +1,8 @@
 function primeCheck (num) {
 	if (num <= 1)
-		return false
-	else if (num <= 3)
-		return true
+		return false;
+	else if (num == 2)
+		return true;
 	else {
 		for (var i = 2; i < num; i++) {
 			if (num % i == 0)
