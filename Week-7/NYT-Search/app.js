8f6e85318861932ff8c1fa9149e330232d9003db
Andrew Murphy <andrewmurphywastaken@gmail.com>
2018-06-09 10:33:50 -0400

    phase 2 of NYT article search

--- Week-7/NYT-Search/app.js
+++ Week-7/NYT-Search/app.js
@@ -1,13 +1,28 @@
-$(document).ready(function() {
-	$("body").on("click", "#submit-button", function() {
-		var keyword = $("#keyword").val();
-		var articleNumber = $("#article-number").val();
-		var startYear = $("#start-year").val();
-		var endYear = $("#end-year").val();
-	
-		console.log(keyword);
-		console.log(articleNumber);
-		console.log(startYear);
-		console.log(endYear);
-	});
+$(document).ready(function () {
+    $("body").on("click", "#submit-button", function () {
+        var keyword = $("#keyword").val();
+        var articleNumber = $("#article-number").val();
+        var startYear = $("#start-year").val();
+        var endYear = $("#end-year").val();
+
+        var queryUrl = "https://api.nytimes.com/svc/search/v2/articlesearch.json?q=" + keyword + "&begin_date=" + startYear + "0101&end_date=" + endYear + "1231&api-key=5f22c86521c340aaad440dea7cbb612f";
+        console.log(queryUrl);
+
+
+        $.ajax({
+            url: queryUrl,
+            method: 'GET',
+        }).then(function (response) {
+            console.log(response.response.docs[0].headline.main);
+            console.log(response.response.docs[0].snippet);
+            console.log(response.response.docs[0].web_url);
+            $("#articletitle").text(response.response.docs[0].headline.main);
+            $("#articlesnippet").text(response.response.docs[0].snippet);
+            $("#articlelink").text(response.response.docs[0].web_url);
+        }).fail(function (err) {
+            throw err;
+        });
+
+
+    });
 });
