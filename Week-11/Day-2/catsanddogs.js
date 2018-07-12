54c3ff8d545a8b73322adcffa8350e1ba1be5b79
Andrew Murphy <andrewmurphywastaken@gmail.com>
2018-07-12 18:59:17 -0400

    completed catsanddogs activity

---
+++ Week-11/Day-2/catsanddogs.js
@@ -0,0 +1,21 @@
+var dogs = {
+	raining: true,
+	noise: "Woof!",
+	makeNoise: function() {
+		if (this.raining)
+			console.log(this.noise);
+	}
+};
+
+var cats = {
+	raining: false,
+	noise: "Meow!",
+	makeNoise: function() {
+		if (this.raining)
+			console.log(this.noise);
+	}
+};
+
+dogs.makeNoise();
+cats.raining = true;
+cats.makeNoise();
