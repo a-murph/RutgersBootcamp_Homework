b1d7696e60f8aa2a95da0ded5d2f11005bd42333
Andrew Murphy <andrewmurphywastaken@gmail.com>
2018-08-09 20:46:48 -0400

    initilization

--- Week-15/Day-2/starWars/app/config/connection.js
+++ Week-15/Day-2/starWars/app/config/connection.js
@@ -3,32 +3,19 @@
 // *********************************************************************************
 
 // Requiring mysql package
-var mysql = require("mysql");
+var Sequelize = require("sequelize");
 
 // Setting up our connection information
-var source = {
-  localhost: {
-    host: "localhost",
-    port: 3306,
-    user: "root",
-    password: "password",
-    database: "starwars"
-  }
-};
-
-
-// Creating our connection
-var connection = mysql.createConnection(source.localhost);
-
-
-// Connecting to the database.
-connection.connect(function(err) {
-  if (err) {
-    console.error("error connecting: " + err.stack);
-    return;
-  }
-  console.log("connected as id " + connection.threadId);
+var sequelize = new Sequelize("starwars", root, "LengthmattersmorethanGirth", {
+	host: "localhost",
+	port: 3306,
+	dialect: "mysql",
+	pool: {
+		max: 5,
+		min: 0,
+		idle: 10000
+	}
 });
 
 // Exporting our connection
-module.exports = connection;
+module.exports = sequelize;
--- Week-15/Day-2/starWars/app/routes/api-routes.js
+++ Week-15/Day-2/starWars/app/routes/api-routes.js
@@ -4,47 +4,56 @@
 
 // Dependencies
 // =============================================================
-var orm = require("../config/orm.js");
+var character = require("../model/character.js");
 
 
 // Routes
 // =============================================================
-module.exports = function(app) {
+module.exports = function (app) {
 
-  // Search for Specific Character (or all characters) then provides JSON
-  app.get("/api/:characters?", function(req, res) {
+	// Search for Specific Character (or all characters) then provides JSON
+	app.get("/api/:characters?", function (req, res) {
 
-    // If the user provides a specific character in the URL...
-    if (req.params.characters) {
+		// If the user provides a specific character in the URL...
+		if (req.params.characters) {
 
-      // Then display the JSON for ONLY that character.
-      // (Note how we're using the ORM here to run our searches)
-      orm.searchCharacter(req.params.characters, function(data) {
-        res.json(data);
-      });
-    }
+			// Then display the JSON for ONLY that character.
+			// (Note how we're using the ORM here to run our searches)
+			//   orm.searchCharacter(req.params.characters, function(data) {
+			//     res.json(data);
+			//   });
 
-    // Otherwise...
-    else {
-      // Otherwise display the data for all of the characters.
-      // (Note how we're using the ORM here to run our searches)
-      orm.allCharacters(function(data) {
-        res.json(data);
-      });
-    }
+			var data = character.findAll({
+				where: {
+					routeName: req.params.characters
+				}
+			});
+		}
 
-  });
+		// Otherwise...
+		else {
+			// Otherwise display the data for all of the characters.
+			// (Note how we're using the ORM here to run our searches)
+			//   orm.allCharacters(function(data) {
+			//     res.json(data);
+			//   });
 
-  // If a user sends data to add a new character...
-  app.post("/api/new", function(req, res) {
+			var data = character.findAll();
+		}
 
-    // Take the request...
-    var character = req.body;
+		res.json(data);
+	});
 
-    // Then send it to the ORM to "save" into the DB.
-    orm.addCharacter(character, function(data) {
-      console.log(data);
-    });
+	// If a user sends data to add a new character...
+	app.post("/api/new", function (req, res) {
 
-  });
+		// Take the request...
+		var character = req.body;
+
+		// Then send it to the ORM to "save" into the DB.
+		// orm.addCharacter(character, function(data) {
+		//   console.log(data);
+		// });
+
+	});
 };
