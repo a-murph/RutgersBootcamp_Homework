49ac79585cef3f937b487569493009ab20f7bb60
Andrew Murphy <andrewmurphywastaken@gmail.com>
2018-08-11 10:02:23 -0400

    attempted sequelizeLibrary activity

--- Week-15/Day-2/sequelizeLibrary/app/models/book.js
+++ Week-15/Day-2/sequelizeLibrary/app/models/book.js
@@ -2,15 +2,25 @@
 // =============================================================
 
 // Require the sequelize library
+var Sequelize = require("sequelize");
 // Require the connection to the database (connection.js)
+var sequelize = require("../config/connection.js");
 
 // Create a "Book" model with the following configuration
-
+var Book = sequelize.define({
+	id: {type: Sequelize.INTEGER, allowNull: false, autoIncrement: true, primaryKey: true},
+	title: {type: Sequelize.STRING, allowNull: false},
+	author: {type: Sequelize.STRING, allowNull: false},
+	genre: {type: Sequelize.STRING, allowNull: false},
+	pages: {type: Sequelize.INTEGER, allowNull: false}
+});
 // 1. A title property of type STRING
 // 2. An author property of type STRING
 // 3. A genre property of type STRING
 // 4. A pages property of type INTEGER
 
 // Sync model with DB
+Book.sync();
 
 // Export the book model for other files to use
+module.exports = Book;
