367df3dafc1b4bde9b513d3ddae38f8a765540e3
Andrew Murphy <andrewmurphywastaken@gmail.com>
2018-08-11 13:34:29 -0400

    completed Create/Read/Update/Delete activities for toDo app

--- Week-15/Day-3/ORMToSequelize/routes/api-routes.js
+++ Week-15/Day-3/ORMToSequelize/routes/api-routes.js
@@ -5,9 +5,7 @@
 // Dependencies
 // =============================================================
 
-// grab the orm from the config
-// (remember: connection.js -> orm.js -> route file)
-// var orm = require("../config/orm.js");
+var db = require("../models");
 
 // Routes
 // =============================================================
@@ -15,30 +13,43 @@ module.exports = function (app) {
 
 	// GET route for getting all of the todos
 	app.get("/api/todos", function (req, res) {
-		// orm.getTodos(function (results) {
-		// 	res.json(results);
-		// });
+		db.ToDo.findAll({}).then(function (dbTodo) {
+			res.json(dbTodo);
+		});
 	});
 
 	// POST route for saving a new todo. We can create a todo using the data on req.body
 	app.post("/api/todos", function (req, res) {
-		// orm.addTodo(req.body, function (results) {
-		// 	res.json(results);
-		// });
+		db.ToDo.create({
+			text: req.body.text,
+		}).then(function(dbTodo) {
+			res.json(dbTodo);
+		});
 	});
 
 	// DELETE route for deleting todos. We can access the ID of the todo to delete in
 	// req.params.id
 	app.delete("/api/todos/:id", function (req, res) {
-		// orm.deleteTodo(req.params.id, function (results) {
-		// 	res.json(results);
-		// });
+		db.ToDo.destroy({
+			where: {
+				id: req.params.id
+			}
+		}).then(function(dbTodo) {
+			res.json(dbTodo);
+		});
 	});
 
 	// PUT route for updating todos. We can access the updated todo in req.body
 	app.put("/api/todos", function (req, res) {
-		// orm.editTodo(req.body, function (results) {
-		// 	res.json(results);
-		// });
+		db.ToDo.update({
+			text: req.body.text,
+			complete: req.body.complete
+		}, {
+			where: {
+				id: req.body.id
+			}
+		}).then(function(dbTodo) {
+			res.json(dbTodo);
+		});
 	});
 };
