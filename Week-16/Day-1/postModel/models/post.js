3d1e7d02276f06cf371952e00fd9febae7924c3c
Andrew Murphy <andrewmurphywastaken@gmail.com>
2018-08-14 18:47:11 -0400

    completed postModel activity

--- Week-16/Day-1/postModel/models/post.js
+++ Week-16/Day-1/postModel/models/post.js
@@ -1,5 +1,30 @@
-module.exports = function(sequelize, DataTypes) {
-  // Add code here to create a Post model
-  // This model needs a title, a body, and a category
-  // Don't forget to 'return' the post after defining
+module.exports = function (sequelize, DataTypes) {
+	var Post = sequelize.define("post", {
+		id: {
+			type: DataTypes.INTEGER,
+			allowNull: false,
+			autoIncrement: true,
+			primaryKey: true
+		},
+		title: {
+			type: DataTypes.STRING,
+			allowNull: false,
+			validate: {
+				len: [1, 160]
+			}
+		},
+		body: {
+			type: DataTypes.TEXT,
+			allowNull: false,
+			validate: {
+				len: [1]
+			}
+		},
+		category: {
+			type: DataTypes.STRING,
+			defaultValue: "Personal"
+		}
+	});
+
+	return Post;
 };
