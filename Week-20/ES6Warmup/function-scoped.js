f6a1b93abe938e67624105d9f4b531ba00bb0f0e
Andrew Murphy <andrewmurphywastaken@gmail.com>
2018-09-11 19:13:24 -0400

    completed ES6 rewrite of var-example

---
+++ Week-20/ES6Warmup/function-scoped.js
@@ -0,0 +1,16 @@
+let name = "Nick";
+
+var tinyize = function(name) {
+  const myName = "Tiny " + name + "!";
+
+  return myName;
+};
+
+name = tinyize("Rick");
+console.log(name);
+
+for (let i = 0; i < 5; i++) {
+  setTimeout(function() {
+    console.log(i);
+  }, 100);
+}
