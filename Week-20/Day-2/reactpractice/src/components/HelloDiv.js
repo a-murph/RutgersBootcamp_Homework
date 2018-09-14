a1093258d5f91c031b1b8df66b737184ea0fcb6b
Andrew Murphy <andrewmurphywastaken@gmail.com>
2018-09-13 20:51:34 -0400

    completed helloDiv activity

---
+++ Week-20/Day-2/reactpractice/src/components/HelloDiv.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+const HelloDiv = () => (
+	<div>
+		<h1>Hello! My name is Andrew.</h1>
+		<p>I like:</p>
+		<ul>
+			<li>Billy Talent</li>
+			<li>Monster Hunter: World</li>
+			<li>Fullmetal Alchemist</li>
+		</ul>
+	</div>
+);
+
+export default HelloDiv;
