aa99a7b8182224b0e254ece07c5c2e20b6fa1015
Andrew Murphy <andrewmurphywastaken@gmail.com>
2018-09-15 13:38:05 -0400

    attempted friendcard refactoring

--- Week-20/Day-3/reactpractice/src/App.js
+++ Week-20/Day-3/reactpractice/src/App.js
@@ -1,31 +1,36 @@
 import React from "react";
-import Wrapper from "./components/Wrapper";
-import Title from "./components/Title";
 import FriendCard from "./components/FriendCard";
+import Wrapper from "./components/Wrapper";
 import friends from "./friends.json";
+import "./App.css";
 
-const App = () => (
-  <Wrapper>
-    <Title>Friends List</Title>
-	<FriendCard
-		name={friends[0].name}
-		image={friends[0].image}
-		job={friends[0].occupation}
-		location={friends[0].location}
-	/>
-	<FriendCard
-		name={friends[1].name}
-		image={friends[1].image}
-		job={friends[1].occupation}
-		location={friends[1].location}
-	/>
-	<FriendCard
-		name={friends[2].name}
-		image={friends[2].image}
-		job={friends[2].occupation}
-		location={friends[2].location}
-	/>
-  </Wrapper>
-);
+class App extends React.Component {
+	state = {
+		friends: friends
+	};
+
+	removeFriend = (id) => {
+
+	}
+
+	render() {
+		return (
+			<Wrapper>
+				{this.state.friends.map(function(friend) {
+				return (
+					<FriendCard
+						removeFriend = {this.removeFriend()}
+						id={friend.id}
+						name={friend.name}
+						image={friend.image}
+						occupation={friend.occupation}
+						location={friend.location}
+					/>
+				);
+				})}
+			</Wrapper>
+		);
+	}
+}
 
 export default App;
--- Week-20/Day-3/reactpractice/srcFriendCard/components/FriendCard/FriendCard.js
+++ Week-20/Day-3/reactpractice/srcFriendCard/components/FriendCard/FriendCard.js
@@ -1,10 +1,13 @@
 import React from "react";
 import "./FriendCard.css";
 
-const FriendCard = props => (
+const FriendCard = (props) => (
   <div className="card">
     <div className="img-container">
-      <img alt={props.name} src={props.image} />
+      <img
+        alt={props.name}
+        src={props.image}
+      />
     </div>
     <div className="content">
       <ul>
@@ -12,14 +15,13 @@ const FriendCard = props => (
           <strong>Name:</strong> {props.name}
         </li>
         <li>
-          <strong>Occupation:</strong> {props.occupation}
+          <strong>Occupation:</strong> {props.job}
         </li>
         <li>
-          <strong>Address:</strong> {props.location}
+          <strong>Location:</strong> {props.location}
         </li>
       </ul>
     </div>
-    <span className="remove" onClick={() => props.removeFriend(props.id)}>𝘅</span>
   </div>
 );
 
